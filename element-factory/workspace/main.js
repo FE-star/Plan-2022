15682d27a720914070a8ec330126801ffbff9605
miniflycn <miniflycn@justany.net>
2022-08-02 15:35:18 +0800

    update

--- element-factory/workspace/main.js
+++ element-factory/workspace/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import Delegator from './app.tpl'
-// import Delegator from './card.tpl'
+// import Delegator from './app.tpl'
+import Delegator from './card.tpl'
 import data from './data/data.mjs'
 
-// const app = createApp(Delegator, data.data.result[0])
-const app = createApp(Delegator, data.data)
+const app = createApp(Delegator, data.data.result[0])
+// const app = createApp(Delegator, data.data)
 app.mount('#app')
