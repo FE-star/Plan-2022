4ccb094c0336f4d427bdf9363db01b59a3813320
xiang.shen <xiang.shen@shopee.com>
2022-08-26 18:40:49 +0800

    fix: 修改了 element.mjs 判断的顺序

--- element-factory/src/nodes/element.mjs
+++ element-factory/src/nodes/element.mjs
@@ -2,12 +2,12 @@ import bind from './bind.mjs'
 const startWithCapacity = /^[A-Z]/
 
 export default (generator, node) => {
-    if (startWithCapacity.test(node.tag)) {
+    if (node.tag.indexOf('Tpl_') > -1) {
+        generator.addDpe(node.tag)
+    } else if (startWithCapacity.test(node.tag)) {
         node.props.forEach(prop => {
             prop.type === 7 && bind(generator, prop)
         })
-        generator.addDpe(node.tag)
-    } else if (node.tag.indexOf('Tpl_') > -1) {
         generator.addDpe(node.tag)
     }
 }
