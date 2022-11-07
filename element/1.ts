f88f3681becc4a7c9911b17ee1c07e4d7b17e805
miniflycn <miniflycn@justany.net>
2022-11-08 02:29:54 +0800

    clean up

--- element/1.ts
+++
@@ -1,8 +0,0 @@
-const cheerio = require('cheerio');
-const $ = cheerio.load('<h2 class="title">Hello world</h2>');
-
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
-
-$.html();
-//=> <html><head></head><body><h2 class="title welcome">Hello there!</h2></body></html>
